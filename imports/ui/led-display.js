db15607d35ef6418e92ca7ff77903ecb3decb520
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-13 07:47:39 -0700

    flushing after long hiatus

--- imports/ui/led-display.js
+++ imports/ui/led-display.js
@@ -38,9 +38,10 @@ export default class LedDisplay {
   }
 
   applyMbostockMagic(leds) {
-    const svgContainer = d3.select('#led-svg').attr('viewBox', '0 0 1200 900').style('background-color', 'black');
-    const rowGroups = svgContainer.selectAll('g').data(leds)
-    rowGroups.enter().append('g').attr('transform', (d, i) => {return `translate(50, ${50 + (i * 100)})`});
+    const svg = d3.select('#led-svg').attr('viewBox', '0 0 1200 900').style('background-color', 'black');
+    const ledContainer = svg.select('#led-container').attr('transform', 'translate(50, 50)'); // .style('filter', 'url(#blur)');
+    const rowGroups = ledContainer.selectAll('g').data(leds);
+    rowGroups.enter().append('g').attr('transform', (d, i) => {return `translate(0, ${i * 100})`});
 
     const circles = rowGroups.merge(rowGroups).selectAll('circle').data((d) => { return d;});
     circles.enter().append('circle').attr('r', 25).attr('cx', (d, i) => {return i * 100;});
--- led_programs/program.c
+++ led_programs/program.c
@@ -10,18 +10,36 @@ extern "C" {
 CHSV leds[Y_LENGTH][X_LENGTH];
 uint16_t frameCounter = 0;
 
-void loop() {
-  uint8_t hue = (frameCounter % 512) / 2;
+void sweepingRainbow() {
+  uint8_t startHue = (frameCounter % 256);
   for (int y = 0; y < Y_LENGTH; y++) {
-    fill_rainbow(leds[y], X_LENGTH, hue);
-  }
+    uint8_t thisStartHue = startHue + (10 * y);
 
-  frameCounter++;
+    fill_rainbow(leds[y], X_LENGTH, thisStartHue, 10);
+  }
 }
 
-void loopAndPopulateArray(uint8_t *array) {
-  loop();
+void rotatingRainbow() {
+  const uint8_t HUE_VARIANCE = 20;
+  uint8_t angle = 256 - (frameCounter % 1792) / 7;
+  uint8_t startHue = (frameCounter % 512) / 2;
+  float yHueDist = ((-128.0 + cos8(angle)) / 128.0) * HUE_VARIANCE;
+  float xHueDist = ((-128.0 + sin8(angle)) / 128.0) * HUE_VARIANCE;
 
+  for (int y = 0; y < Y_LENGTH; y++) {
+    uint8_t thisStartHue = startHue + (yHueDist * y);
+    for (int x = 0; x < X_LENGTH; x++) {
+      uint8_t thisHue = thisStartHue + (xHueDist * x);
+      leds[y][x] = CHSV(thisHue, 255, 255);
+    }
+  }
+}
+
+void generateFrame() {
+  rotatingRainbow();
+}
+
+void translateToLinear(uint8_t *array) {
   int i = 0;
 
   for (int y = 0; y < Y_LENGTH; y++) {
@@ -33,6 +51,12 @@ void loopAndPopulateArray(uint8_t *array) {
       array[i++] = led.b;
     }
   }
+}
+
+void loopAndPopulateArray(uint8_t *array) {
+  frameCounter++;
+  generateFrame();
+  translateToLinear(array);
 }
 
 }
