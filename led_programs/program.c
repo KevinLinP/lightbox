ef385cf9686f63ba7cb02cad7bf6a8545d5a7d67
Kevin Lin <kevin.lin.p@gmail.com>
2018-08-21 21:00:41 -0700

    arrgh ?

--- led_programs/program.c
+++ led_programs/program.c
@@ -23,16 +23,19 @@ void rotatingRainbow() {
   /*uint8_t startHue = (frameCounter % 1024) / 4;*/
   uint8_t startHue = 0;
   uint8_t angle = (frameCounter % 512) / 2;
-  /*int8_t xHueDelta = sin8(angle) - 128;*/
-  int8_t xHueDelta = 0;
-  uint8_t yHueDelta = cos8(angle);
+  uint8_t xHueDelta = cos8(angle);
+  uint8_t yHueDelta = sin8(angle);
 
-  CHSV rowStartColors[Y_LENGTH];
   if (yHueDelta >= 128) {
     yHueDelta = 255 - yHueDelta;
   }
+  
+  if (xHueDelta >= 128) {
+    xHueDelta = 255 - xHueDelta;
+  }
 
-  if (angle <= 127) {
+  CHSV rowStartColors[Y_LENGTH];
+  if (angle >= 64 && angle <= 191) {
     fill_gradient(rowStartColors, 0, CHSV(startHue, 255, 255), Y_LENGTH, CHSV((startHue + yHueDelta), 255, 255));
   } else {
     fill_gradient(rowStartColors, 0, CHSV(startHue - yHueDelta, 255, 255), Y_LENGTH, CHSV((startHue), 255, 255));
@@ -41,10 +44,10 @@ void rotatingRainbow() {
   for (int y = 0; y < Y_LENGTH; y++) {
     CHSV rowStartColor = rowStartColors[y];
 
-    if (xHueDelta >= 0) {
-      fill_gradient(leds[y], 0, rowStartColor, X_LENGTH, CHSV(rowStartColor.hue + xHueDelta, 255, 255), FORWARD_HUES);
+    if (angle <= 127) {
+      fill_gradient(leds[y], 0, rowStartColor, X_LENGTH, CHSV(rowStartColor.hue + xHueDelta, 255, 255));
     } else {
-      fill_gradient(leds[y], 0, CHSV(rowStartColor.hue + xHueDelta, 255, 255), X_LENGTH, rowStartColor, FORWARD_HUES);
+      fill_gradient(leds[y], 0, CHSV(rowStartColor.hue - xHueDelta, 255, 255), X_LENGTH, rowStartColor);
     }
   }
 }
